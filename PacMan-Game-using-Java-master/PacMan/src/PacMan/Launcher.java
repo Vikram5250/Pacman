fc3123d252add8e340780e09ab65b4b22bb4cdb4
Vikram5250 <149860947+Vikram5250@users.noreply.github.com>
2024-04-16 11:37:08 +0530

    Add files via upload

---
+++ PacMan-Game-using-Java-master/PacMan/src/PacMan/Launcher.java
@@ -0,0 +1,16 @@
+
+package PacMan;
+
+public class Launcher {
+
+    
+    public static void main(String[] args) {
+       
+        
+        Game game = new Game("PacMan !!",800,800);
+        game.start();
+    }
+    
+}
+
+
